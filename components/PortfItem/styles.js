7d0827c52091a6ef5c1793233c2bd2315c73db88
Turo1111 <matiaszurita1998@gmail.com>
2022-05-24 14:25:16 -0300

    new port 2

--- components/PortfItem/styles.js
+++ components/PortfItem/styles.js
@@ -11,6 +11,19 @@ export const Container = styled.div `
 export const List = styled.ul `
     margin: 0;
     padding: 10px;
+    max-height: 84%;
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+     width: 5px;
+ }
+ ::-webkit-scrollbar-track {
+     box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+     border-radius: 10px;
+ }
+ ::-webkit-scrollbar-thumb {
+     border-radius: 10px;
+     box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
+ }
 `
 
 export const ListItem = styled.li`
